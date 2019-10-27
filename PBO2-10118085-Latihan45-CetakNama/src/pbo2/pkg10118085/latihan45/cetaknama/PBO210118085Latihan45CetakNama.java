0fa985534cc8080a3d909912de4fe985fbf22f72
Risky Irfansyah <55643683+RiskyIrfansyah@users.noreply.github.com>
2019-10-27 10:51:40 +0700

    Add files via upload

---
+++ PBO2-10118085-Latihan45-CetakNama/src/pbo2/pkg10118085/latihan45/cetaknama/PBO210118085Latihan45CetakNama.java
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pbo2.pkg10118085.latihan45.cetaknama;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author
+ * NAMA     : Risky Irfansyah
+ * KELAS    : IF-2
+ * NIM      : 10118085
+ * Deskripsi Program : Membuat program berbasis object untuk mencetak nama
+ */
+public class PBO210118085Latihan45CetakNama {
+
+    /**
+     * @param args the command line arguments
+     */
+      public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        Printer printer = new Printer();
+        
+        System.out.println("=====Aplikasi Pencetak Nama=====");
+        System.out.print("Masukkan nama anda : ");
+        printer.setNama(scanner.nextLine());
+        System.out.print("Mau cetak nama berapa kali? : ");
+        printer.setJmlCetak(scanner.nextInt());
+        
+        printer.cetak(printer.getNama());
+        printer.cetak(printer.getJmlCetak(), printer.getNama());
+    }
+}
